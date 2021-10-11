6837fdbd72dc8a3762be344bfe047acdb44c3801
LakshmiUX <telidevaralaxmi@gmail.com>
2021-10-11 07:15:10 -0500

    I created a test class

--- TestingOneFacts/src/testFactList.java
+++ TestingOneFacts/src/testFactList.java
@@ -1,4 +1,24 @@
+import static org.junit.Assert.*;
+import org.junit.*;
 
 public class testFactList {
+	@Test
+	
+	public void testSearch() {
+		
+		
+	}
+	
+
+	private void assertTrue(String string, Object search) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static Object search(String string, int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
